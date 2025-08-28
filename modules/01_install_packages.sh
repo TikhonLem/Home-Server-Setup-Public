018492d3fa9147f687f0cf6a493620d8579747c5
TikhonLem <tikhon.server@mail.ru>
2025-08-28 19:11:15 +0300

    Create 01_install_packages.sh

---
+++ modules/01_install_packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ./utils/logger.sh
+
+log "[*] Updating system..."
+apt update >> "$LOG_FILE" 2>&1 && apt upgrade -y >> "$LOG_FILE" 2>&1
+
+log "[*] Installing required packages..."
+apt install -y \
+  fail2ban cockpit cockpit-machines \
+  python3 python3-pip python3-venv \
+  libvirt-daemon-system libvirt-clients virtinst \
+  ufw net-tools curl >> "$LOG_FILE" 2>&1
