aaa726de29967c660a1ab338ae67b1fca2ca22cb
TikhonLem <tikhon.server@mail.ru>
2025-08-30 21:30:14 +0300

    Update 01_install_packages.sh

--- modules/01_install_packages.sh
+++ modules/01_install_packages.sh
@@ -10,4 +10,5 @@ apt install -y \
   fail2ban cockpit cockpit-machines \
   python3 python3-pip python3-venv \
   libvirt-daemon-system libvirt-clients virtinst \
-  ufw net-tools curl >> "$LOG_FILE" 2>&1
+  ufw net-tools curl \
+  btop bat atuin eza >> "$LOG_FILE" 2>&1
